7a8e657965c9f8a32b62f64fd10aaac85348b909
Alexis Checura <alexischecura@gmail.com>
2023-06-26 09:38:14 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { styled } from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+`;
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button>Check In </Button>
+        <Button>Check Out</Button>
+        <Input type="number" placeholder="Number of guest" />
+      </StyledApp>
+    </>
+  );
+}
+
+export default App;
