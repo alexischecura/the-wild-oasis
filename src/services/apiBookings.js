610f63b19d415ae3d40b7e210ef7f628f8645ad9
Alexis Checura <alexischecura@gmail.com>
2023-06-30 19:58:13 -0300

    feat: pagination for bookings

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,12 +1,15 @@
+import { PAGE_SIZE } from "../utils/constants";
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
-export async function getBookings({ filter, sortBy }) {
+export async function getBookings({ filter, sortBy, page }) {
   let query = supabase
     .from("bookings")
     .select(
-      "id, created_at, startDate, endDate, numNights, numGuests, status, total, cabins(name), guests(fullName, email)"
+      "id, created_at, startDate, endDate, numNights, numGuests, status, total, cabins(name), guests(fullName, email)",
+      { count: "exact" }
     );
+
   if (filter) query = query[filter.method || "eq"](filter.field, filter.value);
 
   if (sortBy) {
@@ -15,13 +18,20 @@ export async function getBookings({ filter, sortBy }) {
     });
   }
 
-  const { data, error } = await query;
+  if (page) {
+    const from = (page - 1) * PAGE_SIZE;
+    const to = from + PAGE_SIZE - 1;
+    query = query.range(from, to);
+  }
+
+  const { data, error, count } = await query;
 
   if (error) {
     console.error(error);
     throw new Error("Bookings could not be loaded");
   }
-  return data;
+
+  return { data, count };
 }
 
 export async function getBooking(id) {
@@ -35,8 +45,7 @@ export async function getBooking(id) {
     console.error(error);
     throw new Error("Booking not found");
   }
-
-  return data;
+  return { data };
 }
 
 // Returns all BOOKINGS that are were created after the given date. Useful to get bookings created in the last 30 days, for example.
