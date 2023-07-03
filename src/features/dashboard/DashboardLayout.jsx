9855928f45636b08a12f53f756e43a51f2a95d77
Alexis Checura <alexischecura@gmail.com>
2023-07-03 18:39:32 -0300

    feat: add dark mode with toogle button

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,3 +6,16 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  return (
+    <StyledDashboardLayout>
+      <div>Statistics</div>
+      <div>Today's activities</div>
+      <div>Charrt stay duration</div>
+      <div>Chart Sales</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
